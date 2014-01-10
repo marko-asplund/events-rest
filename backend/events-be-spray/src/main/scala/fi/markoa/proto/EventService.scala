cee27369dae6f8ed0e587579c689423898e657c8
marko asplund <marko.asplund@ixonos.com>
2014-01-11 01:47:00 +0200

    implement REST API routing + JSON object parsing

--- backend/events-be-spray/src/main/scala/fi/markoa/proto/EventService.scala
+++ backend/events-be-spray/src/main/scala/fi/markoa/proto/EventService.scala
@@ -4,21 +4,65 @@ import akka.actor.Actor
 import spray.routing._
 import spray.http._
 import MediaTypes._
+import spray.json.DefaultJsonProtocol
+
+import java.util.Date
+import java.text.SimpleDateFormat
+import java.text.DateFormat
+
+import spray.json.JsonFormat
+import spray.json.JsString
+import spray.json.JsValue
 
 class EventServiceActor extends Actor with EventService {
   def actorRefFactory = context
   def receive = runRoute(myRoute)
 }
 
+case class Event(id: String, title: String, category: String, description: Option[String],
+    startTime: Date, duration: Int)
+
+trait MyJsonProtocol extends DefaultJsonProtocol {
+// FIXME: perf, thread-safety, yoda?, error handling
+implicit object DateJsonFormat extends JsonFormat[Date] {
+    val df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
+    def write(x: Date) = JsString(df.format(x))
+    def read(value: JsValue) = value match {
+      case JsString(x) => df.parse(x.replace("Z", "+0000"))
+      case x => spray.json.deserializationError("Expected String as JsString, but got " + x)
+    }
+  }
+}
+    
+object MyJsonProtocol extends MyJsonProtocol {
+  implicit val EventFormat = jsonFormat6(Event)
+}
+
+import MyJsonProtocol._
+import spray.httpx.SprayJsonSupport._
+
 trait EventService extends HttpService {
   val myRoute =
-    path("") {
+    path("events") {
       get {
         respondWithMediaType(`text/plain`) {
           complete {
-		"hello, world"
+            "get"
           }
         }
+      } ~
+      post {
+        complete("post")
+      }
+    } ~
+    path("events" / IntNumber) { eventId =>
+      entity(as[Event]) { person => 
+      put {
+        complete("put: "+eventId+", "+person)
+      } ~
+      delete {
+        complete("del: "+eventId)
+      }
       }
     }
 }
---
+++ backend/events-be-spray/src/main/scala/fi/markoa/proto/EventServiceDaemon.scala
@@ -0,0 +1,17 @@
+package fi.markoa.proto
+
+class EventServiceDaemon {
+
+  def init(args: Array[String]) {
+  }
+
+  def start() {
+  }
+
+  def stop() {
+  }
+
+  def destroy() {
+  }
+  
+}
