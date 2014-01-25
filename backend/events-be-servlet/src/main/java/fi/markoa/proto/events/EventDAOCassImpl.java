3bf8b34a30eb125140b6841ade939985b8ee3486
marko asplund <marko.asplund@ixonos.com>
2014-01-25 17:29:32 +0200

    events-be-servlet: convert anon classes to Java 8 Lambda expressions

--- backend/events-be-servlet/src/main/java/fi/markoa/proto/events/EventDAOCassImpl.java
+++ backend/events-be-servlet/src/main/java/fi/markoa/proto/events/EventDAOCassImpl.java
@@ -1,6 +1,7 @@
 package fi.markoa.proto.events;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,34 +41,20 @@ public class EventDAOCassImpl implements EventDAO {
 	@Override
   public ListenableFuture<String> create(Event e) {
     final UUID id = UUIDs.random();
-    Function<ResultSet, String> transformation = new Function<ResultSet, String>() {
-      public String apply(ResultSet rs) {
-        return id.toString();
-      }
-    };
-
+    Function<ResultSet, String> transformation = (rs) -> id.toString();
     return createOrUpdate(id, e, transformation);
 	}
 	
 	@Override
   public ListenableFuture<Event> read(String id) {
-    Function<ResultSet, Event> transformation = new Function<ResultSet, Event>() {
-      public Event apply(ResultSet rs) {
-        return eventFromRow(rs.one());
-      }
-    };
-
+    Function<ResultSet, Event> transformation = (rs) -> eventFromRow(rs.one());
     ResultSetFuture rsf = session.executeAsync(statements.get("read").bind(UUID.fromString(id)));
     return Futures.transform(rsf, transformation);
 	}
 	
 	@Override
   public ListenableFuture<Void> update(String id, Event e) {
-    Function<ResultSet, Void> transformation = new Function<ResultSet, Void>() {
-      public Void apply(ResultSet rs) {
-        return null;
-      }
-    };
+    Function<ResultSet, Void> transformation = (rs) -> null;
 	  return createOrUpdate(UUID.fromString(id), e, transformation);
 	}
 	
@@ -75,25 +62,14 @@ public class EventDAOCassImpl implements EventDAO {
 	@Override
   public ListenableFuture<Void> delete(final String id) {
     final ResultSetFuture rsf = session.executeAsync(statements.get("delete").bind(UUID.fromString(id)));
-	  Function<ResultSet, Void> transformation = new Function<ResultSet, Void>() {
-      public Void apply(ResultSet rs) {
-        return null;
-      }
-    };
+	  Function<ResultSet, Void> transformation = (rs) -> null;
     return Futures.transform(rsf, transformation);
 	}
 	
 	@Override
   public ListenableFuture<List<Event>> list() {
-    Function<ResultSet, List<Event>> transformation = new Function<ResultSet, List<Event>>() {
-      public List<Event> apply(ResultSet rs) {
-        List<Event> events = new ArrayList<>();
-        for(Row r : rs.all()) {
-          events.add(eventFromRow(r));
-        }
-        return events;
-      }
-    };
+    Function<ResultSet, List<Event>> transformation = (rs) ->
+      rs.all().stream().map( this::eventFromRow ).collect(Collectors.toList());
 		ResultSetFuture rsf = session.executeAsync(statements.get("list").bind());
 		return Futures.transform(rsf, transformation);
 	}
@@ -111,7 +87,7 @@ public class EventDAOCassImpl implements EventDAO {
 	}
 	
   private <T> ListenableFuture<T> createOrUpdate(final UUID id, Event e, Function<ResultSet, T> transformation) {
-    BoundStatement bs = null;
+    BoundStatement bs;
     if(e.getDescription() == null)
       bs = statements.get("create1").bind(id, e.getTitle(), e.getCategory(), e.getStartTime(), new Integer(e.getDuration()));
     else
@@ -119,6 +95,5 @@ public class EventDAOCassImpl implements EventDAO {
     ResultSetFuture rsf = session.executeAsync(bs);
     return Futures.transform(rsf, transformation);
   }
-
 
 }
